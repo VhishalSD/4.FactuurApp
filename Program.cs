863bb62a4b1a5e59f45c5be6bef9c32117d5fef4
Vhishal Kewal <vkhehim@gmail.com>
2025-07-17 19:52:07 +0200

    fixed euro sign

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace FactuurApp
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             // Set default culture to Dutch (Netherlands) for euro symbol support and date formatting
             CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("nl-NL");
             CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("nl-NL");
